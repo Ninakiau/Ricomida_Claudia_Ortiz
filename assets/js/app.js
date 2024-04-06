ae3ba78ba5b47031667eefef1ac9f1aec685d889
Claudia <claudia.ortiz@uc.cl>
2024-04-06 01:55:46 -0300

    add: adding button addfavorites

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,25 +2,27 @@
 $(document).ready(function () {
   // Esto inicializa el tooltip
   $("#enviarCorreo").tooltip();
+  $("#addFavorite").tooltip();
 
   // Agrega una acción al hacer clic en el botón
   $("#enviarCorreo").click(function () {
     // Enviar alerta
     alert("El correo se ha enviado correctamente");
   });
+
+  $("#addFavorite").click(function () {
+    // Enviar alerta
+    alert("La receta se ha agregado a tus favoritos");
+  });
 });
 
 // Cambiar color de texto al hacer doble clic en el título "INGREDIENTES" y "PREPARACIÓN"
-$(document).ready(function () {
-  // Agregar evento dblclick al título "INGREDIENTES"
-  $("#ingredients").on("dblclick", function () {
-    $(this).toggleClass("red-text");
-  });
+$("#ingredients").on("dblclick", function () {
+  $(this).toggleClass("red-text");
+});
 
-  // Agregar evento dblclick al título "PREPARACIÓN"
-  $("#preparation").on("dblclick", function () {
-    $(this).toggleClass("red-text");
-  });
+$("#preparation").on("dblclick", function () {
+  $(this).toggleClass("red-text");
 });
 
 //Desaparecer texto al hacer clic en el título de la tarjeta
